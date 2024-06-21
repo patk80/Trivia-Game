e19da317bbc5a4b2fb896257018816af98726f51
Pat Kelly <PatK80@gmail.com>
2024-06-21 16:39:53 -0400

    JS declaring variables

--- scripts.js
+++ scripts.js
@@ -9,13 +9,24 @@
 
 // addQuestionMark(exString, qVar);
 
+let question    = document.getElementById("question");
+let answerOne   = document.getElementById("answer-1");
+let answerTwo   = document.getElementById("answer-2");
+let answerThree = document.getElementById("answer-3");
+let answerFour  = document.getElementById("answer-4");
 
 
 let question1 = {
-    question: "what is this?",
-    answer1: "a1",
-    answer2: "a2",
-    answer3: "a3",
-    answer4: "a4",
-    correctAnswer: answer3,
+    question: "q1 - what is this?",
+    answer1: "q1 a1",
+    answer2: "q1 a2",
+    answer3: "q1 a3",
+    answer4: "q1 a4",
+    // correctAnswer: answer3
 }
+
+question.innerHTML = question1.question;
+answerOne.innerHTML = question1.answer1;
+answerTwo.innerHTML = question1.answer2;
+answerThree.innerHTML = question1.answer3;
+answerFour.innerHTML = question1.answer4;
