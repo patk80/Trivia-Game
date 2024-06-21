5e8f0a23dab65640955ebfa010ec02983f38213b
Pat Kelly <PatK80@gmail.com>
2024-06-21 17:09:06 -0400

    randomQuestion function working properly

--- scripts.js
+++ scripts.js
@@ -3,32 +3,48 @@ let answerOneText   = document.getElementById("answer-1");
 let answerTwoText   = document.getElementById("answer-2");
 let answerThreeText = document.getElementById("answer-3");
 let answerFourText  = document.getElementById("answer-4");
+let scoreText       = document.getElementById("score");
 
-let allQuestions = [
+let allQuestionsArray = [
     {
-    question: "q1 - what is this?",
-    answers: {
-        a: "q1 a1",
-        b: "q1 a2",
-        c: "q1 a3",
-        d: "q1 a4",
-      },
-      correctAnswer: 'c'
+        question: "q1 - what is this?",
+        answers: {
+            a: "q1 a1",
+            b: "q1 a2",
+            c: "q1 a3",
+            d: "q1 a4",
+        },
+        correctAnswer: 'c'
     },
     {
-    question: "q2 - wsdjsdjlsjdldksd?",
-    answers: {
-        a: "q2 a1",
-        b: "q2 a2",
-        c: "q2 a3",
-        d: "q2 a4",
-        },
-        correctAnswer: 'd'
-    }
-]
+        question: "q2 - wsdjsdjlsjdldksd?",
+        answers: {
+            a: "q2 a1",
+            b: "q2 a2",
+            c: "q2 a3",
+            d: "q2 a4",
+            },
+            correctAnswer: 'd'
+    },
+    {
+        question: "q3 - WOLOLO!!!",
+        answers: {
+            a: "q3 a1",
+            b: "q3 a2",
+            c: "q3 a3",
+            d: "q3 a4",
+            },
+            correctAnswer: 'd'
+        }
+];
 
-questionText.innerHTML    = allQuestions[0].question;
-answerOneText.innerHTML   = allQuestions[0].answers.a;
-answerTwoText.innerHTML   = allQuestions[0].answers.b;
-answerThreeText.innerHTML = allQuestions[0].answers.c;
-answerFourText.innerHTML  = allQuestions[0].answers.d;
+function randomQuestion() {
+    let randomIndex = Math.floor( Math.random() * (allQuestionsArray.length) );
+    questionText.innerHTML    = allQuestionsArray[randomIndex].question;
+    answerOneText.innerHTML   = allQuestionsArray[randomIndex].answers.a;
+    answerTwoText.innerHTML   = allQuestionsArray[randomIndex].answers.b;
+    answerThreeText.innerHTML = allQuestionsArray[randomIndex].answers.c;
+    answerFourText.innerHTML  = allQuestionsArray[randomIndex].answers.d;
+}
+
+randomQuestion();
