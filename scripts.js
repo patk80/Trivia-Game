c9af3aa7b32e137834acb58811ea15142bb7eac9
Pat Kelly <PatK80@gmail.com>
2024-06-21 16:26:52 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// let exString = "Sup Rick"
+// let qVar = "?"
+
+// function addQuestionMark(string, variable){
+//     for(let i = 0; i < 10; i++){
+//         console.log(string += variable);
+//     }
+// }
+
+// addQuestionMark(exString, qVar);
+
+
+
