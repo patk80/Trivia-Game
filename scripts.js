b8e4d038bbac7eac7b4116d712198a92da584d8e
Pat Kelly <PatK80@gmail.com>
2024-06-22 12:39:58 -0400

    userGuess func, logging correct answer, switching to a/b/c/d

--- scripts.js
+++ scripts.js
@@ -1,42 +1,42 @@
-let questionText       = document.querySelector("#question");
-let answerOneText      = document.querySelector("#answer-1");
-let answerTwoText      = document.querySelector("#answer-2");
-let answerThreeText    = document.querySelector("#answer-3");
-let answerFourText     = document.querySelector("#answer-4");
-let scoreText          = document.querySelector("#score"); 
-let submitButton       = document.querySelector("#submit-button"); 
-let allButtons         = document.querySelectorAll(".btn");
+let questionText     = document.querySelector("#question");
+let answerOneText    = document.querySelector("#answer-a");
+let answerTwoText    = document.querySelector("#answer-b");
+let answerThreeText  = document.querySelector("#answer-c");
+let answerFourText   = document.querySelector("#answer-d");
+let scoreText        = document.querySelector("#score"); 
+let submitButton     = document.querySelector("#submit-button"); 
+let allAnswerButtons = document.querySelectorAll(".answer-btn");
 
 let allQuestionsArray = [
     {
         question: "q1 - what is this?",
         answers: {
-            a: "q1 a1",
-            b: "q1 a2",
-            c: "q1 a3",
-            d: "q1 a4",
+            a: "q1 wrong 1",
+            b: "q1 wrong 2",
+            c: "q1 CORRECT",
+            d: "q1 wrong 3",
         },
-        correctAnswer: 'c'
+        correctAnswer: "c"
     },
     {
         question: "q2 - wsdjsdjlsjdldksd?",
         answers: {
-            a: "q2 a1",
-            b: "q2 a2",
-            c: "q2 a3",
-            d: "q2 a4",
+            a: "q2 wrong 1",
+            b: "q2 wrong 2",
+            c: "q2 wrong 3",
+            d: "q2 CORRECT",
             },
-            correctAnswer: 'd'
+            correctAnswer: "d"
     },
     {
         question: "q3 - WOLOLO!!!",
         answers: {
-            a: "q3 a1",
-            b: "q3 a2",
-            c: "q3 a3",
-            d: "q3 a4",
+            a: "q3 CORRECT",
+            b: "q3 wrong 1",
+            c: "q3 wrong 2",
+            d: "q3 wrong 3",
             },
-            correctAnswer: 'd'
+            correctAnswer: "a"
         }
 ];
 
@@ -47,30 +47,45 @@ function randomQuestion() {
     answerTwoText.innerHTML   = allQuestionsArray[randomIndex].answers.b;
     answerThreeText.innerHTML = allQuestionsArray[randomIndex].answers.c;
     answerFourText.innerHTML  = allQuestionsArray[randomIndex].answers.d;
+
+
+    console.log("************ from randomQuestionsArray");
+    console.log(allQuestionsArray[randomIndex].correctAnswer);
+    console.log("************ from randomQuestionsArray");
     // remove current question from allQuestionsArray with splice //
     allQuestionsArray.splice([randomIndex], 1);
 }
 
 // Messing around with selected item by user //
-answerOneText.addEventListener("click", function() {
-    answerOneText.style.color = "magenta";
-    answerOneText.style.fontWeight = 'bold';
-});
+// answerOneText.addEventListener("click", function() {
+//     answerOneText.style.color = "magenta";
+//     answerOneText.style.fontWeight = 'bold';
+// });
 
-answerTwoText.addEventListener("click", function() {
-    answerTwoText.style.color = "yellow";
-    answerTwoText.style.fontWeight = 'bold';
-});
+// answerTwoText.addEventListener("click", function() {
+//     answerTwoText.style.color = "yellow";
+//     answerTwoText.style.fontWeight = 'bold';
+// });
 
-answerThreeText.addEventListener("click", function() {
-    answerThreeText.style.color = "red";
-    answerThreeText.style.fontWeight = 'bold';
-});
+// answerThreeText.addEventListener("click", function() {
+//     answerThreeText.style.color = "red";
+//     answerThreeText.style.fontWeight = 'bold';
+// });
 
-answerFourText.addEventListener("click", function() {
-    answerFourText.style.color = "blue";
-    answerFourText.style.fontWeight = 'bold';
-});
+// answerFourText.addEventListener("click", function() {
+//     answerFourText.style.color = "blue";
+//     answerFourText.style.fontWeight = 'bold';
+// });
+
+
+function userGuess(){
+    for(i of allAnswerButtons) {
+        i.addEventListener("click", function(){
+            console.log(this.id);
+        });
+    }
+}
+userGuess();
 
 
 randomQuestion();
